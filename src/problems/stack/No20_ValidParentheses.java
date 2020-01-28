e5e9f014ee22cdfe34921943a692a62ef47a7107
Kuangshi Yan <kyan8897@163.com>
2020-01-28 17:29:59 +0800

    add problem 20

---
+++ src/problems/stack/No20_ValidParentheses.java
@@ -0,0 +1,123 @@
+package problems.stack;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * 20. 有效的括号
+ * 难度：简单
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ *
+ * @author kyan
+ * @date 2020/1/28
+ */
+public class No20_ValidParentheses {
+
+    /**
+     * 思路：
+     * 利用栈，遍历字符数组，当遇到'('，'['，'{'时入栈，遇到')'，']'，'}'时判断栈是否为空（为空则可以判断为false），或栈顶字符
+     * 是否匹配，如果匹配则出栈，否则判断为false
+     * 遍历完成后最后判断栈是否为空，如果字符都是一一匹配的话，那么栈一定是空的
+     *
+     * time 3ms, beat 59.6%
+     * space 34.1MB, beat 80.05%
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(' || chars[i] == '{' || chars[i] == '[') {
+                stack.push(chars[i]);
+            }
+            switch (chars[i]) {
+                case ')' :
+                    if (!stack.isEmpty() && stack.peek() == '(') {
+                        stack.pop();
+                        break;
+                    } else {
+                        return false;
+                    }
+                case ']' :
+                    if (!stack.isEmpty() && stack.peek() == '[') {
+                        stack.pop();
+                        break;
+                    } else {
+                        return false;
+                    }
+                case '}' :
+                    if (!stack.isEmpty() && stack.peek() == '{') {
+                        stack.pop();
+                        break;
+                    } else {
+                        return false;
+                    }
+                 default:
+                     break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public HashMap<Character, Character> mappings = new HashMap<Character, Character>() {
+        {
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }
+    };
+
+    public boolean isValid2(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < chars.length; i++) {
+            if (mappings.containsKey(chars[i])) {
+                //如果为')','}',']'之一，则需要判断栈顶元素是否与之匹配
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (!stack.pop().equals(mappings.get(chars[i]))) {
+                    return false;
+                }
+            } else {
+                stack.push(chars[i]);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()";
+        No20_ValidParentheses solution = new No20_ValidParentheses();
+        System.out.println(solution.isValid2(s));
+    }
+}
