2143a883a8c4b26dda7fd74da79372dd3f9dde40
Kuangshi Yan <kyan8897@163.com>
2020-02-14 17:51:33 +0800

    76题解题优化

--- src/problems/string/_76_MinWindowSubstring.java
+++ src/problems/string/_76_MinWindowSubstring.java
@@ -6,17 +6,17 @@ import java.util.Map;
 /**
  * 76. 最小覆盖子串
  * 难度：困难
- *
+ * <p>
  * 题目描述：
- *
+ * <p>
  * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字母的最小子串。
- *
+ * <p>
  * 示例：
- *
+ * <p>
  * 输入: S = "ADOBECODEBANC", T = "ABC"
  * 输出: "BANC"
  * 说明：
- *
+ * <p>
  * 如果 S 中不存这样的子串，则返回空字符串 ""。
  * 如果 S 中存在这样的子串，我们保证它是唯一的答案。
  *
@@ -35,36 +35,79 @@ public class _76_MinWindowSubstring {
             Map<Character, Integer> needMap = new HashMap<>();
             Map<Character, Integer> windowMap = new HashMap<>();
             int len = s.length(), minLen = len + 1;
-            int left = 0, right = 0, match = 0;
+            int l = 0, r = 0, match = 0;
             String res = "";
-            for (Character c : t.toCharArray()) {
+            for (char c : t.toCharArray()) {
                 needMap.put(c, needMap.getOrDefault(c, 0) + 1);
             }
-            while (left < len && right < len) {
-                Character charToAdd = s.charAt(right);
-                if (needMap.containsKey(charToAdd)) {
-                    windowMap.put(charToAdd, windowMap.getOrDefault(charToAdd, 0) + 1);
+            while (l < len && r < len) {
+                char ch = s.charAt(r);
+                if (needMap.containsKey(ch)) {
+                    windowMap.put(ch, windowMap.getOrDefault(ch, 0) + 1);
 //                    if (windowMap.get(charToAdd) == needMap.get(charToAdd)) {
                     //以上 == 判断在长字符串情况下报错
-                    if (windowMap.get(charToAdd).compareTo(needMap.get(charToAdd)) == 0) {
+                    if (windowMap.get(ch).compareTo(needMap.get(ch)) == 0) {
                         match++;
                     }
                 }
                 while (match == needMap.size()) {
-                    if (right - left + 1 < minLen) {
-                        minLen = right - left + 1;
-                        res = s.substring(left, right + 1);
+                    if (r - l + 1 < minLen) {
+                        minLen = r - l + 1;
+                        res = s.substring(l, r + 1);
                     }
-                    Character charToRemove = s.charAt(left);
-                    if (needMap.containsKey(charToRemove)) {
-                        windowMap.put(charToRemove, windowMap.get(charToRemove) - 1);
-                        if (windowMap.get(charToRemove) < needMap.get(charToRemove)) {
+                    ch = s.charAt(l);
+                    if (needMap.containsKey(ch)) {
+                        windowMap.put(ch, windowMap.get(ch) - 1);
+                        if (windowMap.get(ch) < needMap.get(ch)) {
                             match--;
                         }
                     }
-                    left++;
+                    l++;
                 }
-                right++;
+                r++;
+            }
+            return res;
+        }
+    }
+
+    /**
+     * 执行用时：7 ms, 76.60%
+     * 内存消耗：45.5 MB, 5.02%
+     */
+    public static class Solution2 {
+
+        public String minWindow(String s, String t) {
+            int[] needIndex = new int[128];
+            int[] windowIndex = new int[128];
+            int len = s.length(), minLen = len + 1;
+            int l = 0, r = 0, match = 0;
+            String res = "";
+            for (char c : t.toCharArray()) {
+                needIndex[c] = needIndex[c] + 1;
+            }
+            while (r < len) {
+                char ch = s.charAt(r);
+                if (needIndex[ch] > 0) {
+                    windowIndex[ch] = windowIndex[ch] + 1;
+                    if (windowIndex[ch] <= needIndex[ch]) {
+                        match++;
+                    }
+                }
+                while (match == t.length()) {
+                    if (r - l + 1 < minLen) {
+                        minLen = r - l + 1;
+                        res = s.substring(l, r + 1);
+                    }
+                    ch = s.charAt(l);
+                    if (needIndex[ch] > 0) {
+                        windowIndex[ch] = windowIndex[ch] - 1;
+                        if (windowIndex[ch] < needIndex[ch]) {
+                            match--;
+                        }
+                    }
+                    l++;
+                }
+                r++;
             }
             return res;
         }
@@ -73,8 +116,11 @@ public class _76_MinWindowSubstring {
     public static void main(String[] args) {
 //        String S = "cabwefgewcwaefgcf";
 //        String T = "cae";
-        String S = "a";
-        String T = "aa";
+//        String S = "a";
+//        String T = "aa";
+        String S = "ADOBECODEBANC";
+        String T = "AABC";
         System.out.println(new Solution1().minWindow(S, T));
+        System.out.println(new Solution2().minWindow(S, T));
     }
 }
